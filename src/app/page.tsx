7e2180bdc2dd94a304f2cd2b7330e242c74aa84e
zekepari <pariezekiel@gmail.com>
2024-05-16 21:10:16 +1000

    make it look pretty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,34 @@
 import Image from "next/image";
-import EvesBrand from "./_assets/EvesBrand.svg"
 import EvesApple from "./_assets/EvesApple.svg"
-import Ceiling from "./_assets/Ceiling.svg"
 import Vine1 from "./_assets/Vine1.svg"
 import Vine2 from "./_assets/Vine2.svg"
 import Vine3 from "./_assets/Vine3.svg"
+import Vine4 from "./_assets/Vine4.svg"
+import Vine5 from "./_assets/Vine5.svg"
+import Vine6 from "./_assets/Vine6.svg"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24">
-      <Image src={Ceiling} alt="Vine Ceiling" className="absolute w-full -z-10 -inset-y-20 xl:block hidden" />
-      <div className="absolute -z-20 w-1/2 left-0 -inset-y-0 top-96 overflow-hidden">
-        <Image src={Vine1} alt="Vine" className="absolute w-full xl:block hidden" style={{ left: '-40%' }} />
+    <>
+      <div className="absolute flex xl:justify-around justify-center w-full items-start overflow-hidden">
+        <Image src={Vine1} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine2} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine3} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine4} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine5} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine6} alt="Eve's Games Brand" className="rotate-180" />
       </div>
-      <div className="absolute -z-30 w-2/5 left-0 -inset-y-0 overflow-hidden">
-        <Image src={Vine2} alt="Vine" className="absolute w-full xl:block hidden" style={{ left: '-20%' }} />
-      </div>
-      <div className="absolute -z-40 w-2/5 right-0 top-0 bottom-0 overflow-hidden">
-        <Image src={Vine3} alt="Vine" className="absolute w-full xl:block hidden" style={{ right: '-30%' }} />
-      </div>
+      <main className="flex min-h-screen flex-col items-center justify-between py-24">
+        <div/>
 
-      <div className="xl:block hidden" />
+        <Image src={EvesApple} alt="Eve's Games Apple" className="z-10" />
 
-      <div>
-        <Image src={EvesBrand} alt="Eve's Games Brand" className="hidden md:block drop-shadow-xl" />
-        <Image src={EvesApple} alt="Eve's Games Apple" className="md:hidden" />
-      </div>
+        <p className="text-center">
+          Welcome to Eve&apos;s Games.<br />
+          <a href="https://discord.gg/uBjR4r7Msq" target="_blank" className="text-blue-500 hover:text-blue-400">Discord ↗</a>
+        </p>
+      </main>
+    </>
 
-      <p className="text-center">
-        Welcome to Eve&apos;s Games.<br />
-        <a href="https://discord.gg/uBjR4r7Msq" target="_blank" className="text-blue-500 hover:text-blue-400">Discord ↗</a>
-      </p>
-    </main>
   );
 }
