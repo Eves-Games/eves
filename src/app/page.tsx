b80034a537c1a853f2e62f8254b29873637e3e4b
zekepari <pariezekiel@gmail.com>
2024-05-16 21:18:06 +1000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import EvesApple from "./_assets/EvesApple.svg"
 import Vine1 from "./_assets/Vine1.svg"
+import Vine2 from "./_assets/Vine2.svg"
 import Vine3 from "./_assets/Vine3.svg"
 import Vine4 from "./_assets/Vine4.svg"
 import Vine5 from "./_assets/Vine5.svg"
@@ -11,19 +12,20 @@ export default function Home() {
     <>
       <div className="absolute flex xl:justify-around justify-center w-full items-start overflow-hidden">
         <Image src={Vine1} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine2} alt="Eve's Games Brand" className="rotate-180" />
         <Image src={Vine3} alt="Eve's Games Brand" className="rotate-180" />
-        <Image src={Vine6} alt="Eve's Games Brand" className="rotate-180" />
         <Image src={Vine4} alt="Eve's Games Brand" className="rotate-180" />
         <Image src={Vine5} alt="Eve's Games Brand" className="rotate-180" />
+        <Image src={Vine6} alt="Eve's Games Brand" className="rotate-180" />
       </div>
       <main className="flex min-h-screen flex-col items-center justify-between py-24">
         <div/>
 
         <Image src={EvesApple} alt="Eve's Games Apple" className="z-10" />
 
-        <p className="text-center">
+        <p className="text-center z-10">
           Welcome to Eve&apos;s Games.<br />
-          <a href="https://discord.gg/uBjR4r7Msq" target="_blank" className="text-blue-500 hover:text-blue-400 z-10">Discord ↗</a>
+          <a href="https://discord.gg/uBjR4r7Msq" target="_blank" className="text-blue-500 hover:text-blue-400">Discord ↗</a>
         </p>
       </main>
     </>
